7cae83b390bc185a5c295542af1d05da7d2e0e70
shir shponder <shponders@gmail.com>
2024-11-23 19:25:40 +0200

    add new route: delete comment

--- src/routes/commentsRoute.js
+++ src/routes/commentsRoute.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import {
   createComment,
+  deleteCommentById,
   updateCommentById,
 } from '../controllers/commentsController.js';
 
@@ -9,5 +10,7 @@ const router = Router();
 router.post('/', createComment);
 
 router.put('/:id', updateCommentById);
+
+router.delete('/:id', deleteCommentById);
 
 export default router;

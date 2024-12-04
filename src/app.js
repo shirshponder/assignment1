fa8cc1dc22135901976f8060c596fc0341b42392
shir shponder <shponders@gmail.com>
2024-12-04 21:20:43 +0200

    create function createExpress

--- src/app.js
+++ src/app.js
@@ -1,20 +1,9 @@
 import 'dotenv/config';
-import express from 'express';
-import bodyParser from 'body-parser';
-import postsRoute from './routes/postsRoute.js';
-import commentsRoute from './routes/commentsRoute.js';
-import { connectDatabase } from './config/connectToDatabase.js';
+import { createExpress } from './createExpress.js';
 
 const main = async () => {
-  const app = express();
+  const app = await createExpress();
   const port = process.env.PORT;
-  await connectDatabase();
-
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-
-  app.use('/posts', postsRoute);
-  app.use('/comments', commentsRoute);
 
   app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);

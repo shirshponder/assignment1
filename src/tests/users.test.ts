d656b1d1bbdfa7ed3f2749cc6820fe23588ba0c0
Michalikhol <michali.khol@gmail.com>
2025-01-11 21:35:17 +0200

    finished users tests

--- src/tests/users.test.ts
+++ src/tests/users.test.ts
@@ -22,7 +22,7 @@ const testUser: User = {
 };
 
 beforeAll(async () => {
-  console.log('beforeAll');
+  console.log('before all usres tests');
   app = await createExpress();
   await userModel.deleteMany();
 
@@ -40,7 +40,7 @@ beforeAll(async () => {
 });
 
 afterAll((done) => {
-  console.log('afterAll');
+  console.log('after all users tests');
   mongoose.connection.close();
   done();
 });
@@ -50,5 +50,181 @@ describe('Users Tests', () => {
     const response = await requestWithAuth.get('/users');
     expect(response.statusCode).toBe(status.OK);
     expect(response.body.length).toBe(1);
+  });
+
+  test('Create new user', async () => {
+    const testUser2: User = {
+      email: 'test2@user.com',
+      username: 'Mich2',
+      password: 'testpassword2',
+    };
+    const responseNewUser = await requestWithAuth
+      .post('/users')
+      .send(testUser2);
+    expect(responseNewUser.statusCode).toBe(status.OK);
+    expect(responseNewUser.body.email).toBe(testUser2.email);
+    expect(responseNewUser.body.username).toBe(testUser2.username);
+  });
+
+  test('Get user by id', async () => {
+    const response = await requestWithAuth.get(`/users/${testUser._id}`);
+    expect(response.statusCode).toBe(status.OK);
+    expect(response.body._id).toBe(testUser._id);
+  });
+
+  test('Get user by id - not found', async () => {
+    const response = await requestWithAuth.get(
+      '/users/222222222222222222222222'
+    );
+    expect(response.statusCode).toBe(status.NOT_FOUND);
+  });
+
+  test('Get user by email', async () => {
+    const response = await requestWithAuth.get(
+      `/users?email=${testUser.email}`
+    );
+    expect(response.statusCode).toBe(status.OK);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0]._id).toBe(testUser._id);
+  });
+
+  test('Get user by email - not found', async () => {
+    const response = await requestWithAuth.get(`/users?email=notexist@co.il`);
+    expect(response.body.length).toBe(0);
+  });
+
+  test('Get user by username', async () => {
+    const response = await requestWithAuth.get(
+      `/users?username=${testUser.username}`
+    );
+    expect(response.statusCode).toBe(status.OK);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0]._id).toBe(testUser._id);
+  });
+
+  test('Get user by username - not found', async () => {
+    const response = await requestWithAuth.get(`/users?username=notexist`);
+    expect(response.body.length).toBe(0);
+  });
+
+  test('Update user username', async () => {
+    const testUser3: User = {
+      email: 'test3@user.com',
+      username: 'Mich3',
+      password: 'testpassword3',
+    };
+    const responseNewUser = await requestWithAuth
+      .post('/users')
+      .send(testUser3);
+    expect(responseNewUser.statusCode).toBe(status.OK);
+
+    const response = await requestWithAuth
+      .put(`/users/${responseNewUser.body._id}`)
+      .send({ username: 'updateUsername' });
+
+    expect(response.statusCode).toBe(status.OK);
+    expect(response.body.username).toBe('updateUsername');
+
+    await userModel.deleteOne({ _id: responseNewUser.body._id });
+  });
+
+  test('Update user username - existing username', async () => {
+    const testUser3: User = {
+      email: 'test3@user.com',
+      username: 'Mich3',
+      password: 'testpassword3',
+    };
+    const responseNewUser = await requestWithAuth
+      .post('/users')
+      .send(testUser3);
+    expect(responseNewUser.statusCode).toBe(status.OK);
+
+    const response = await requestWithAuth
+      .put(`/users/${testUser._id}`)
+      .send({ username: 'Mich3' });
+    expect(response.statusCode).toBe(status.BAD_REQUEST);
+
+    await userModel.deleteOne({ _id: responseNewUser.body._id });
+  });
+
+  test('Update user password', async () => {
+    const testUser3: User = {
+      email: 'test3@user.com',
+      username: 'Mich3',
+      password: 'testpassword3',
+    };
+    const responseNewUser = await requestWithAuth
+      .post('/users')
+      .send(testUser3);
+    expect(responseNewUser.statusCode).toBe(status.OK);
+    const hashedPassword = responseNewUser.body.password;
+
+    const response = await requestWithAuth
+      .put(`/users/${responseNewUser.body._id}`)
+      .send({ password: 'updatePassword' });
+
+    expect(response.statusCode).toBe(status.OK);
+    expect(response.body.password).not.toBe(hashedPassword);
+
+    await userModel.deleteOne({ _id: responseNewUser.body._id });
+  });
+
+  test('Update user email', async () => {
+    const testUser3: User = {
+      email: 'test3@user.com',
+      username: 'Mich3',
+      password: 'testpassword3',
+    };
+    const responseNewUser = await requestWithAuth
+      .post('/users')
+      .send(testUser3);
+    expect(responseNewUser.statusCode).toBe(status.OK);
+
+    const response = await requestWithAuth
+      .put(`/users/${responseNewUser.body._id}`)
+      .send({ email: 'updateMail@user.com' });
+    expect(response.statusCode).toBe(status.OK);
+    expect(response.body.email).toBe('updateMail@user.com');
+
+    await userModel.deleteOne({ _id: responseNewUser.body._id });
+  });
+
+  test('Update user email - existing email', async () => {
+    const testUser3: User = {
+      email: 'test3@user.com',
+      username: 'Mich3',
+      password: 'testpassword3',
+    };
+    const responseNewUser = await requestWithAuth
+      .post('/users')
+      .send(testUser3);
+    expect(responseNewUser.statusCode).toBe(status.OK);
+
+    const response = await requestWithAuth
+      .put(`/users/${testUser._id}`)
+      .send({ email: 'test3@user.com' });
+    expect(response.statusCode).toBe(status.BAD_REQUEST);
+
+    await userModel.deleteOne({ _id: responseNewUser.body._id });
+  });
+
+  test('Delete user', async () => {
+    const testUser3: User = {
+      email: 'test3@user.com',
+      username: 'Mich3',
+      password: 'testpassword3',
+    };
+    const responseNewUser = await requestWithAuth
+      .post('/users')
+      .send(testUser3);
+    expect(responseNewUser.statusCode).toBe(status.OK);
+
+    const response = await requestWithAuth.delete(
+      `/users/${responseNewUser.body._id}`
+    );
+    expect(response.statusCode).toBe(status.OK);
+    expect(response.body._id).toBe(responseNewUser.body._id);
+
+    await userModel.deleteOne({ _id: responseNewUser.body._id });
   });
 });

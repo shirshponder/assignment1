ebbfbf911a9326a042b182dc089d28ae46233127
Michalikhol <michali.khol@gmail.com>
2025-01-12 20:07:53 +0200

    added swagger

--- src/routes/postsRoute.ts
+++ src/routes/postsRoute.ts
@@ -3,14 +3,223 @@ import postsController from '../controllers/postsController';
 
 const router = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Posts
+ *   description: The posts API
+ */
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ *   schemas:
+ *     Post:
+ *       type: object
+ *       required:
+ *         - content
+ *         - sender
+ *         - title
+ *       properties:
+ *         content:
+ *           type: string
+ *           description: The Post content
+ *         sender:
+ *           type: string
+ *           description: The Post sender
+ *         title:
+ *           type: string
+ *           description: The Post title
+ *       example:
+ *         content: 'This is a Post'
+ *         sender: 'shir'
+ *         title: 'Shirs post'
+ */
+
+/**
+ * @swagger
+ * /posts:
+ *   get:
+ *     summary: Get all posts
+ *     description: Returns the list of all the Posts or filterd by sender
+ *     tags:
+ *       - Posts
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *      - in: query
+ *        name: sender
+ *        schema:
+ *          type: string
+ *        required: false
+ *        description: The sender ID to filter by
+ *     responses:
+ *       '200':
+ *         description: A list of the posts or filterd
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Post'
+ *       '500':
+ *         description: Server error
+ */
 router.get('/', postsController.getAll.bind(postsController));
 
+/**
+ * @swagger
+ * /posts/{id}:
+ *   get:
+ *     summary: Get post by ID
+ *     description: Returns single post
+ *     tags:
+ *       - Posts
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: ID of the post
+ *     responses:
+ *       '200':
+ *         description: A post
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *       '404':
+ *         description: Post not found
+ *       '500':
+ *         description: Server error
+ */
 router.get('/:id', postsController.getById.bind(postsController));
 
+/**
+ * @swagger
+ * /posts:
+ *   post:
+ *     summary: Create a new post
+ *     description: Create a new post
+ *     tags:
+ *       - Posts
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - title
+ *               - content
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 description: Title of the post
+ *               sender:
+ *                 type: string
+ *                 description: The sender of the post
+ *               content:
+ *                 type: string
+ *                 description: The content of the post
+ *     responses:
+ *       '201':
+ *         description: Post created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *       '500':
+ *         description: Server error
+ */
 router.post('/', postsController.create.bind(postsController));
 
+/**
+ * @swagger
+ * /posts/{id}:
+ *   delete:
+ *     summary: Delete a post by ID
+ *     description: Delete a single post by its ID
+ *     tags:
+ *       - Posts
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The ID of the post
+ *     responses:
+ *       '200':
+ *         description: Post deleted successfully
+ *       '404':
+ *         description: Post not found
+ *       '500':
+ *         description: Server error
+ */
 router.delete('/:id', postsController.deleteItem.bind(postsController));
 
+/**
+ * @swagger
+ * /posts/{id}:
+ *   put:
+ *     summary: Update a post
+ *     description: Update a post
+ *     tags:
+ *       - Posts
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: ID of the post
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             oneOf:
+ *               - type: object
+ *                 required:
+ *                   - content
+ *                 properties:
+ *                   content:
+ *                     type: string
+ *                     description: The new content of the post
+ *               - type: object
+ *                 required:
+ *                   - title
+ *                 properties:
+ *                   title:
+ *                     type: string
+ *                     description: The new title of the post
+ *     responses:
+ *       '200':
+ *         description: Post updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *       '404':
+ *         description: Post not found
+ *       '500':
+ *         description: Server error
+ */
 router.put('/:id', postsController.updateItem.bind(postsController));
 
 export default router;
